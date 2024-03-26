2664a15c2fa5eee3affeafc88cc2aad4c6b9580d
kul-sudo <ulyanov.kirill@protonmail.com>
2024-03-27 01:06:48 +0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl -LJO https://raw.githubusercontent.com/kul-sudo/canoon/main/canoon
+curl -LJO -p "canoon$RANDOM" https://raw.githubusercontent.com/kul-sudo/canoon/main/canoon
 chmod +x canoon
 sudo ./canoon
 rm -rf ./canoon
